ebe74faee4b8b8174af4acd8a5164fb000b3598d
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2023-05-10 14:35:40 +0200

    Implemented SmoothGen

--- pSIDH.py
+++ pSIDH.py
@@ -1,7 +1,8 @@
+from itertools import combinations
 from sage.all import ZZ
 
-from sqisign.KLPT import RepresentIntegerHeuristic
-from sqisign.setup import Bτ, eτ, p, ω, l
+from sqisign.KLPT import EquivalentPrimeIdealHeuristic, EquivalentRandomEichlerIdeal, RepresentIntegerHeuristic
+from sqisign.setup import B, Bτ, O0, eτ, p, ω, l
 from sqisign.utilities import inert_prime
 
 
@@ -34,3 +35,36 @@ def ConnectingIdeal(O1, O2):
 
     I = O1 * γ + O2 * Nτ
     return I
+
+
+def SmoothGen(O, D):
+    # N = a subset of the natural numbers following certain constraints
+    # O = maximal order
+    # D = prime
+    # Returns a generating family theta_1, theta_2, theta_3 for Z + DO, where n(theta_j) is in N
+
+    L = set()
+    I0 = ConnectingIdeal(O0, O)
+
+    target_order = ZZ + D * O
+
+    found_fam = False
+    generating_fam = []
+    while not found_fam:
+        I = EquivalentPrimeIdealHeuristic(I0) # Same as RandomEquivalentPrimeIdeal()
+        while I is None:
+            I = EquivalentPrimeIdealHeuristic(I0) # Same as RandomEquivalentPrimeIdeal()
+
+        alpha = I / I0
+        theta = EquivalentRandomEichlerIdeal(I, D) # Same as EichlerSuborderNormEquation()
+
+        L.add(alpha * theta * alpha^(-1))
+
+        if len(L) >= 3:
+            for trial_generating_fam in combinations(L, 3):
+                if target_order == B.ideal([1] + trial_generating_fam):
+                    found_fam = True
+                    generating_fam = trial_generating_fam
+                    break
+
+    return generating_fam
